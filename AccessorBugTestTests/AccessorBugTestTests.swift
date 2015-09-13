0e1fb4477df48db8880464a18bb7c16ebbdcbe00
David Gavilan <david.gavilan.ruiz@gmail.com>
2015-09-13 11:15:30 +0200

    Tidy up comments.

--- AccessorBugTestTests/AccessorBugTestTests.swift
+++ AccessorBugTestTests/AccessorBugTestTests.swift
@@ -2,7 +2,7 @@
 //  AccessorBugTestTests.swift
 //  AccessorBugTestTests
 //
-//  Created by Matt Neuburg on 9/12/15.
+//  Created by David Gavilan on 9/11/15.
 //  Copyright © 2015 David Gavilan. All rights reserved.
 //
 
@@ -13,11 +13,9 @@ class AccessorBugTestTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
@@ -31,7 +29,7 @@ class AccessorBugTestTests: XCTestCase {
         XCTAssertEqual(st0, st1, "These two should be equivalent!")
         XCTAssertEqual(st1, st2, "These two should be equivalent!")
         // This fails in Release only, but not in Debug
-        // note from matt: I'm not seeing failure here
+        // note from matt: I'm not seeing failure here in Xcode 7 GM
         XCTAssertEqual(st2, st3, "These two should be equivalent!")
     }
 
